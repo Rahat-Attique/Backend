248f324244738b897aa0ec14b8d0ae046c5f95c5
rahta attique <rahatattique@gmail.com>
2018-11-25 09:23:56 +0500

    backend

---
+++ LoginRegistrationDemo/LoginRegistrationDemo/Controllers/MessStaffController.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using LoginRegistrationDemo.Models;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace LoginRegistrationDemo.Controllers
+{
+    public class MessStaffController : Controller
+    {
+        // GET: MessStaff
+        HostelProjectEntities db = new HostelProjectEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dinner()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult expenses()
+        {
+
+
+            return View();
+        }
+        public ActionResult approvedrequest(ApprovedStudent stu)
+        {
+
+
+            return View(db.ApprovedStudents.ToList());
+        }
+        public ActionResult newrequests(User users)
+        {
+
+            //return View(new User());
+            return View(db.Users.ToList());
+        }
+        public ActionResult lunch()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ LoginRegistrationDemo/LoginRegistrationDemo/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginRegistrationDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+
+        // [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        public ActionResult AddComplains()
+        {
+            return View();
+        }
+
+        public ActionResult ViewDetails()
+        {
+            return View();
+        }
+
+        public ActionResult Calender()
+        {
+            return View();
+        }
+    }
+}
