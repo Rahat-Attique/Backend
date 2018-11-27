bd2bc380672eeef979d68a3f2d7d5af29e281598
rahta attique <rahatattique@gmail.com>
2018-11-27 14:44:29 -0800

    Approved Student function added

--- LoginRegistrationDemo/LoginRegistrationDemo/Controllers/MessStaffController.cs
+++ LoginRegistrationDemo/LoginRegistrationDemo/Controllers/MessStaffController.cs
@@ -13,6 +13,9 @@ namespace LoginRegistrationDemo.Controllers
     {
         // GET: MessStaff
         HostelProjectEntities db = new HostelProjectEntities();
+        public List<User> users { get; set; }
+        public List<ApprovedStudent> approvedStudents { get; set; }
+
 
         public ActionResult Index()
         {
@@ -32,18 +35,49 @@ namespace LoginRegistrationDemo.Controllers
 
             return View();
         }
-        public ActionResult approvedrequest(ApprovedStudent stu)
+
+        public ActionResult approvedrequest(ApprovedStudent j)
         {
 
+            db.ApprovedStudents.Add(j);
 
+            db.SaveChanges();
+            //return RedirectToAction("Index", new { id = 0 });
             return View(db.ApprovedStudents.ToList());
         }
         public ActionResult newrequests(User users)
         {
 
             //return View(new User());
+            db.Users.Add(users);
+
             return View(db.Users.ToList());
         }
+        public ActionResult practice(int ID)
+        {
+            var temps = db.Users.Where(t => t.ID == ID).ToList();
+            ApprovedStudent k = new ApprovedStudent();
+            User obj = db.Users.Find(ID);
+            k.Username = obj.Username;
+            k.Email = obj.Email;
+            k.CNIC = obj.CNIC;
+            k.Degree = obj.Degree;
+            k.Department = obj.Department;
+            k.HostelName = obj.HostelName;
+            k.RoomNumber = obj.RoomNumber;
+            k.phoneNumber = obj.PhoneNumber;
+         //   k.IsValid = obj.IsValid(o)
+
+            k.RegistrationNumber = obj.RegistrationNumber;
+           
+            db.ApprovedStudents.Add(k);
+            db.SaveChanges();
+            return RedirectToAction("newrequests", new { id = 0 });
+           // return View(db.ApprovedStudents.ToList());
+
+        }
+
+
         public ActionResult lunch()
         {
 
--- LoginRegistrationDemo/LoginRegistrationDemo/Controllers/StudentController.cs
+++ LoginRegistrationDemo/LoginRegistrationDemo/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using LoginRegistrationDemo.Models;
+
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,7 +11,9 @@ namespace LoginRegistrationDemo.Controllers
     public class StudentController : Controller
     {
         // GET: Student
-
+        HostelProjectEntities db = new HostelProjectEntities();
+        public List<User> users { get; set; }
+        public List<ApprovedStudent> approvedStudents { get; set; }
         // [HttpPost]
         public ActionResult Index()
         {
@@ -29,6 +33,15 @@ namespace LoginRegistrationDemo.Controllers
 
         public ActionResult Calender()
         {
+            return View();
+        }
+        public ActionResult ViewDetails(int ID)
+        {
+
+            var temps = db.Users.Where(t => t.ID == ID).ToList();
+          //  ApprovedStudent k = new ApprovedStudent();
+            User obj = db.Users.Find(ID);
+
             return View();
         }
     }
